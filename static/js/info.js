7a5d8b1e5966dd70d015eb11e27a3e5f5a62a27c
Software City <davissoftware6@gmail.com>
2021-01-05 23:12:54 +0100

    Delete info.js

--- static/js/info.js
+++
@@ -1,9 +0,0 @@
-var { shell } = require("electron")
-var remote = require("electron").remote
-
-document.getElementById("version").innerText = remote.app.getVersion();
-document.getElementById("dwns").innerText = getCacheVal("downloads");
-
-function openURL(url){
-    shell.openExternal(url)
-}
